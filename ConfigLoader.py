9da626e4a629ad81d0a6bd6f5b79396ffe2e8e7c
echabert <Eric.Chabert@cern.ch>
2021-09-30 23:30:52 +0200

    add ratio plots (for 2 series of data) with error bar + fix empty line in config file

--- ConfigLoader.py
+++ ConfigLoader.py
@@ -25,7 +25,7 @@ def LoadPlots(filename, alpha=0.5):
                 #print("there")
                 if d['type']=='1d' and d.get('xvar',False):
                     #command="{axis}.hist("+str(d.get('xvar'))
-                    command="{axis}.hist(data['"+str(d.get('xvar'))+"']"
+                    command="ybin, edbin, patch = {axis}.hist(data['"+str(d.get('xvar'))+"']"
                     command+=",alpha="+str(alpha)
                     if eval(d.get('ylog','False')) : command+=",log=True"
                     if eval(d.get('norm','False')) : command+=",density=True"
@@ -61,15 +61,82 @@ def AddLegend(ax,nx,ny):
         for j in range(ny):
             ax[i][j].legend()
 
-def FillPlots(filename,fig,ax,nx,ny,label='',**data):
-    commands,nvar = LoadPlots("config/plots.cfg")
+####################################################
+# Compute ratio plots
+# Inputs:
+#  - vbin1/2: bin content
+#  - edbin1/2: edge bins
+#  - ax: matplotlib axis
+# Error computation assumes that vbins corresponds
+# to entries (poisson distribution)
+####################################################
+def RatioPlot(vbin1,edbin1,vbin2,edbin2,ax):
+    # Compute ratio
+    ratio = np.divide(vbin1,vbin2,where=(vbin2 != 0))
+    # Compute error on ratio (null if cannot be computed)
+    error = np.divide(vbin1 * np.sqrt(vbin2) + vbin2 * np.sqrt(vbin1),np.power(vbin2, 2),where=(vbin2 != 0))
+     
+    # Add the ratio on the existing plot
+    ax2 = ax.twinx()
+    ax2.set_ylabel('ratio')
+
+    bincenter = 0.5 * (edbin1[1:] + edbin1[:-1])
+    #ax.errorbar(bincenter, ratio, yerr=error, fmt='.', color='r')
+    # need to call plt and not ax otherwise the y-axis range is wrong
+    plt.errorbar(bincenter, ratio, yerr=error, fmt='.', color='r')
+
+####################################################
+# Add ratio plots for all plots of 2 data series
+# Inputs:
+# - data1/2: list of dictionnary with keys (ybin: #entries, edbin: edge bins)
+# - fig: matplotlib figure
+# - ax: matplotlib axis
+# - nx,ny: dimension of axes array
+####################################################
+def FillRatioPlots(data1,data2,fig,ax,nx,ny):
     counter=1
     ix=0
     iy=0
-    for c in commands.split('\n'):
-        print(c)
-        tmp=""
+    print(len(data1))
+    for i in range(len(data1)):
+        #if i>3: break
+        if ny>1:
+            RatioPlot(data1[i]['ybin'],data1[i]['edbin'],data2[i]['ybin'],data2[i]['edbin'],ax[ix][iy])
+        else:
+            if nx==1 and ny==1:
+                RatioPlot(data1[i]['ybin'],data1[i]['edbin'],data2[i]['ybin'],data2[i]['edbin'],ax)
+            else:
+                RatioPlot(data1[i]['ybin'],data1[i]['edbin'],data2[i]['ybin'],data2[i]['edbin'],ax[ix])
+        if iy<(ny-1): 
+            iy+=1
+        else:
+            ix+=1
+            iy=0
 
+##################################################################
+# Fill the plots on existing figure and subplots
+# inputs: 
+# - config-filename
+# - matplotlib fig
+# - matplotlib (sub)axis
+# - nx,ny, the dimensions of the axis array
+# - label: applied for all plots
+# - data: a dictionnary of variable name (key) and a list(collection) or data
+# output:
+#  return a list of dictionnaries (one per plot)
+#  dict: edge-bins, x-bin-values, y-bin-values
+def FillPlots(filename,fig,ax,nx,ny,label='',**data):
+    commands,nvar = LoadPlots(filename)
+    counter=1
+    ix=0
+    iy=0
+    results=[]
+    ind=1
+    for c in commands.split('\n'):
+        if c == "": continue
+        d={}
+        print("c = ",c)
+        tmp=""
         if ny>1:
             tmp="app = c.format(axis"+"='ax["+str(ix)+"]["+str(iy)+"]'"+",label='"+str(label)+"')"
         else:
@@ -83,10 +150,12 @@ def FillPlots(filename,fig,ax,nx,ny,label='',**data):
             ix+=1
             iy=0
         exec(tmp)
-        print(tmp)
-        print(locals()['app'])
+        #print(tmp)
+        #print(locals())
         exec(locals()['app'])
+        results.append({"edbin":locals()['edbin'],"ybin":locals()['ybin'],"patch":locals()['patch']})
     #plt.show()
+    return results
 
 
 if __name__ == "__main__":
@@ -97,8 +166,8 @@ if __name__ == "__main__":
     ###############################################
     # Test ConfigLoader with
     ###############################################
-
-    commands,nvar = LoadPlots("config/plots.cfg")
+    filename="config/plots.cfg"
+    commands,nvar = LoadPlots(filename)
 
     # Fixing random state for reproducibility
     np.random.seed(19680801)
@@ -120,13 +189,22 @@ if __name__ == "__main__":
     ny=int(m.ceil(nvar/nx))
     fig, ax = plt.subplots(nx,ny)
 
-    FillPlots("config/plots.cfg",fig,ax,nx,ny,label='A',Ih=Ih,eta=eta,p=p,Ias=Ias)
-    Ih2 = np.random.default_rng().standard_gamma(6,npseudo)
+    out1 = FillPlots(filename,fig,ax,nx,ny,label='A',Ih=Ih,eta=eta,p=p,Ias=Ias)
+    Ih2 = np.random.default_rng().standard_gamma(5,npseudo)
     eta2 = np.random.default_rng().normal(0.5, 2, npseudo)
     p2 = np.random.default_rng().exponential(6, npseudo)
     Ias2 = np.random.default_rng().normal(0.3, 0.05, npseudo)
-    FillPlots("config/plots.cfg",fig,ax,nx,ny,label='B',Ih=Ih2,eta=eta2,p=p2,Ias=Ias2)
+    out2 = FillPlots(filename,fig,ax,nx,ny,label='B',Ih=Ih2,eta=eta2,p=p2,Ias=Ias2)
     AddLegend(ax,nx,ny)
+    #print(out1)
+    FillRatioPlots(out1,out2,fig,ax,nx,ny)
+    #fig.suptitle("title") 
+    #print(ax[0][0].__dict__)
+    #xdata = ax[0][0].get_xdata()
+    #ydata = ax[0][0].get_xdata()
+    #print(xdata)
+    #print(ydata)
+    
     plt.show()
     
     exit()
